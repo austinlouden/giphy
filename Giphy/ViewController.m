143ef29951de1f8973633400ceb00bb5276a5926
Austin Louden <austinlouden@gmail.com>
2019-03-27 23:52:05 -0700

    search and refresh the feed

--- Giphy/ViewController.m
+++ Giphy/ViewController.m
@@ -16,8 +16,12 @@
 
 #define GIPHY_API_KEY @""
 
-@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+static CGFloat const kSearchBarHeight = 100.0;
+static CGFloat const kSearchBarPadding = 16.0;
+
+@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UITextFieldDelegate>
 @property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) UITextField *textField;
 @property (nonatomic, strong) NSMutableArray *images;
 @end
 
@@ -37,27 +41,49 @@
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
+    
+    // Create the search bar
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(kSearchBarPadding,
+                                                                   self.view.safeAreaInsets.top + kSearchBarPadding,
+                                                                   CGRectGetWidth(self.view.frame) - kSearchBarPadding,
+                                                                   kSearchBarHeight)];
+    self.textField.delegate = self;
+    self.textField.placeholder = @"Search";
+    self.textField.backgroundColor = [UIColor colorWithWhite:1 alpha:0.5];
+    self.textField.returnKeyType = UIReturnKeySearch;
+    [self.view addSubview:self.textField];
 
     // Create the collection view
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
-    [self.collectionView setDataSource:self];
-    [self.collectionView setDelegate:self];
+    layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    layout.estimatedItemSize = CGSizeMake(1, 1);
+    
+    layout.minimumInteritemSpacing = 0.0;
+    layout.minimumLineSpacing = 0.0;
+    layout.sectionInset = UIEdgeInsetsZero;
+    
+    CGRect frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame) - kSearchBarHeight);
+    
+    self.collectionView = [[UICollectionView alloc] initWithFrame:frame collectionViewLayout:layout];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    self.collectionView.backgroundColor = [UIColor whiteColor];
     [self.collectionView registerClass:[GIFCollectionViewCell class] forCellWithReuseIdentifier:@"GIFCell"];
-    [self.collectionView setBackgroundColor:[UIColor blackColor]];
     [self.view addSubview:self.collectionView];
     
-    [self fetchImages];
+    [self fetchImages:@"ryan gosling"];
 }
 
-- (void)fetchImages
+- (void)fetchImages:(NSString *)query
 {
-    NSDictionary *parameters = @{ @"q": @"ryan gosling", @"limit": @100, @"api_key": GIPHY_API_KEY};
+    NSDictionary *parameters = @{ @"q": query, @"limit": @100, @"api_key": GIPHY_API_KEY};
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     
+    __weak ViewController *weakSelf = self;
     [manager GET:@"https://api.giphy.com/v1/gifs/search?" parameters:parameters progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-        self.images = responseObject[@"data"];
-        [self.collectionView reloadData];
+        weakSelf.images = responseObject[@"data"];
+        [weakSelf.collectionView.collectionViewLayout invalidateLayout];
+        [weakSelf.collectionView reloadData];
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
@@ -74,8 +100,6 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     GIFCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"GIFCell" forIndexPath:indexPath];
-    
-    NSLog(@"%@", self.images[indexPath.row][@"images"][@"fixed_width"]);
     NSURL *url = [NSURL URLWithString:self.images[indexPath.row][@"images"][@"fixed_width"][@"url"]];
     [cell.imageView pin_setImageFromURL:url];
     return cell;
@@ -87,8 +111,17 @@
     CGFloat height = [self.images[indexPath.row][@"images"][@"fixed_width"][@"height"] floatValue];
     CGFloat aspectRatio = width / height;
 
-    return CGSizeMake(CGRectGetWidth(self.view.frame), CGRectGetWidth(self.view.frame) / aspectRatio);
+    return CGSizeMake(CGRectGetWidth(collectionView.frame), CGRectGetWidth(collectionView.frame) / aspectRatio);
 }
 
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self fetchImages:textField.text];
+    [textField resignFirstResponder];
+    [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] atScrollPosition:UICollectionViewScrollPositionTop animated:YES];
+    return NO;
+}
 
 @end
